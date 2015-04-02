9549ae113e31805d71913349d6557d38d4f95a1e
dickeyxxx <jeff@dickeyxxx.com>
2015-04-01 17:19:43 -0700

    added error/warn methods

--- cli.js
+++ cli.js
@@ -1,3 +1,7 @@
+var output = require('./output');
+
 exports.run = require('./run');
 exports.log = console.log;
 exports.formatDate = require('./date').formatDate;
+exports.error = output.error;
+exports.warn = output.warn;
