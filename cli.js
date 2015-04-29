2e428b15193b1fcd4794ecb28e70773b387b4061
Hunter Loftis <hunter@hunterloftis.com>
2015-04-29 13:21:54 -0700

    bind exposed console

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@ var console = require('./console');
 var errors = require('./errors');
 
 exports.run = require('./run');
-exports.log = console.log;
+exports.log = console.log.bind(console);
 exports.formatDate = require('./date').formatDate;
 exports.error = errors.error;
 exports.warn = errors.warn;
