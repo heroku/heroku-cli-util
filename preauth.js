5ecc908f78849f21d9fca0810358b6111054b6ff
dickeyxxx <jeff@dickeyxxx.com>
2015-05-07 16:12:02 -0700

    added 2fa and preauth

--- preauth.js
+++ preauth.js
@@ -1,18 +1,11 @@
 'use strict';
-let prompt = require('./prompt');
 let util   = require('./util');
 
-function preauth (app, heroku) {
-  return new Promise(function (fulfill, reject) {
-    prompt.prompt('Two-factor code', {mask: true}).then(function (second_factor) {
-      fulfill(heroku.request({
-        method: 'PUT',
-        path:   `/apps/${app}/pre-authorizations`,
-        headers: {
-          'Heroku-Two-Factor-Code': second_factor
-        }
-      }));
-    }).catch(reject);
+function preauth (app, heroku, secondFactor) {
+  return heroku.request({
+    method: 'PUT',
+    path:   `/apps/${app}/pre-authorizations`,
+    headers: { 'Heroku-Two-Factor-Code': secondFactor }
   });
 }
 
