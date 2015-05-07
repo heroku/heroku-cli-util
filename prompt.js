e7b6f10250f2c535a9320ab6b6f6b2ad396d8f0d
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-05-06 21:12:00 -0700

    added masking for prompt()

--- prompt.js
+++ prompt.js
@@ -4,11 +4,65 @@ let chalk  = require('chalk');
 let errors = require('./errors');
 let util   = require('./util');
 
-function prompt (name) {
-  return new Promise(function (fulfill) {
-    process.stdin.resume();
+
+function promptMasked (options) {
+  return new Promise(function (fulfill, reject) {
     process.stdin.setEncoding('utf8');
-    process.stderr.write(name ? name + ': ' : '> ');
+    process.stderr.write(options.prompt);
+    process.stdin.resume();
+    process.stdin.setRawMode(true);
+    let input = '';
+    process.stdin.on('data', function (c) {
+      switch(c) {
+      case "\u0004":
+      case "\r":
+      case "\n":
+        if (input.length === 0) { return; }
+        if (options.mask) {
+          process.stderr.clearLine();
+          process.stderr.cursorTo(0);
+          process.stderr.write(options.prompt + input.replace(/./g, '*'));
+        }
+        console.error();
+        process.stdin.setRawMode(false);
+        process.stdin.pause();
+        fulfill(input);
+        return;
+      case "\u0003":
+        // Ctrl-c
+        reject();
+        return;
+      default:
+        if (c.charCodeAt(0) === 127) {
+          // backspace
+          input = input.substr(0, input.length-1);
+          process.stderr.clearLine();
+          process.stderr.cursorTo(0);
+          if (options.mask) {
+            process.stderr.write(options.prompt + input);
+          } else {
+            process.stderr.write(options.prompt + input.replace(/./g, '*'));
+          }
+          return;
+        }
+        process.stderr.write(options.mask ? c : '*');
+        input += c;
+        return;
+      }
+    });
+  });
+}
+
+function prompt (name, options) {
+  options = options || {};
+  options.prompt = name ? name + ': ' : '> ';
+  if (options.mask || options.hide) {
+    return promptMasked(options);
+  }
+  return new Promise(function (fulfill) {
+    process.stdin.setEncoding('utf8');
+    process.stderr.write(options.prompt);
+    process.stdin.resume();
     process.stdin.once('data', function (data) {
       process.stdin.pause();
       data = data.trim();
