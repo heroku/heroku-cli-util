886c376a8291444e4b63e9c8bd596b01a4b12b73
dickeyxxx <jeff@dickeyxxx.com>
2015-05-12 15:28:59 -0700

    fixed host config vars

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 'use strict';
 
 let config = require('rc')('heroku', {
-  color:          true,
-  host:           'heroku.com',
-  git_host:       'heroku.com',
-  http_git_host:  'git.heroku.com',
+  color:  true
 });
+
+config.host          = process.env.HEROKU_HOST || 'heroku.com';
+config.git_host      = process.env.HEROKU_GIT_HOST || config.host;
+config.http_git_host = process.env.HEROKU_HTTP_GIT_HOST || 'git.' + config.host;
 
 module.exports = config;
