64cef41c3344de4924bd9abc35fb2e9f09346b35
dickeyxxx <jeff@dickeyxxx.com>
2015-05-12 16:34:11 -0700

    use ini directly instead of rc for simpler config

--- config.js
+++ config.js
@@ -1,8 +1,21 @@
 'use strict';
 
-let config = require('rc')('heroku', {
-  color:  true
-});
+let fs   = require('fs');
+let path = require('path');
+let _    = require('lodash');
+let ini  = require('ini');
+
+let home = process.env.HOME || process.env.USERPROFILE || '/root';
+let config = {
+  color:  true,
+  home:   home,
+};
+
+try {
+  let p = path.join(home, '.herokurc');
+  let iniConfig = ini.parse(fs.readFileSync(p, 'utf-8'));
+  config = _.assign(config, iniConfig);
+} catch (e) {}
 
 config.host          = process.env.HEROKU_HOST || 'heroku.com';
 config.git_host      = process.env.HEROKU_GIT_HOST || config.host;
