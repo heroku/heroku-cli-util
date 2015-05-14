4012d6ba67898e1be82ff8619c15e324976902a9
dickeyxxx <jeff@dickeyxxx.com>
2015-05-14 12:55:10 -0700

    consolidated test helpers

--- test/helpers.js
+++ test/helpers.js
@@ -1,4 +1,8 @@
 'use strict';
+global.cli = require('heroku-cli-util');
+cli.mockConsole();
+cli.config.raiseErrors = true;
+
 let chai = require('chai');
 chai.should();
 chai.use(require('chai-as-promised'));
--- test/helpers/cli.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-global.cli = require('../..');
-cli.mockConsole();
