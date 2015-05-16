55fbf77eea4a51c64f7870ac77142d8ac946b597
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-05-15 21:54:42 -0700

    fixed tests

--- test/errors.js
+++ test/errors.js
@@ -3,11 +3,11 @@
 describe('errors', function () {
   it('prints out errors', function () {
     cli.error('foobar');
-    cli.stderr.should.contain(' !    foobar\n');
+    cli.color.stripColor(cli.stderr).should.contain(' ▸    foobar\n');
   });
 
   it('prints out warnings', function () {
     cli.warn('foobar');
-    cli.stderr.should.contain(' !    foobar\n');
+    cli.color.stripColor(cli.stderr).should.contain(' ▸    foobar\n');
   });
 });
