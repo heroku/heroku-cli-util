a38455b8a39bd5d997456b4c174dc47b9e1adf14
Ransom Briggs <rbriggs@heroku.com>
2015-11-03 07:53:18 -0600

    Fix bug with special names and disabled colors

--- test/color.js
+++ test/color.js
@@ -6,6 +6,7 @@ let chalk  = require('chalk');
 describe('color', function () {
   beforeEach(function() {
     cli.mockConsole();
+    cli.color.enabled = true;
   });
 
   it('app is cyan', function () {
@@ -18,6 +19,16 @@ describe('color', function () {
 
   it('addon is magenta', function () {
     expect(cli.color.addon('myaddon')).to.equal(chalk.magenta('myaddon'));
+  });
+
+  it('special colors respect supportsColor', function () {
+    cli.color.enabled = false;
+    expect(cli.color.app('myapp')).to.equal('myapp');
+  });
+
+  it('custom colors respect supportsColor', function () {
+    cli.color.enabled = false;
+    expect(cli.color.magenta('myaddon')).to.equal('myaddon');
   });
 
   it('proxies all other colors through to chalk', function () {
