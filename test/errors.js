887e8e8d529d80f47e94ac4d4ddaa03d36e53e65
dickeyxxx <jeff@dickeyxxx.com>
2015-11-05 16:51:20 -0800

    prepend warnings with WARNING:
    otherwise only color differentiates from errors
    
    thoughts @ransombriggs?

--- test/errors.js
+++ test/errors.js
@@ -3,13 +3,15 @@
 let expect = require('chai').expect;
 
 describe('errors', function () {
+  beforeEach(() => cli.mockConsole());
+
   it('prints out errors', function () {
     cli.error('foobar');
-    expect(cli.color.stripColor(cli.stderr)).to.contain(' ▸    foobar\n');
+    expect(cli.color.stripColor(cli.stderr)).to.equal(' ▸    foobar\n');
   });
 
   it('prints out warnings', function () {
     cli.warn('foobar');
-    expect(cli.color.stripColor(cli.stderr)).to.contain(' ▸    foobar\n');
+    expect(cli.color.stripColor(cli.stderr)).to.equal(' ▸    WARNING: foobar\n');
   });
 });
