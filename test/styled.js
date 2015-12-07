8e59dff2d8bb21a2b02067b1fc98b451c733d36f
Tom Crayford <tcrayford@googlemail.com>
2015-12-07 14:25:27 +0000

    add styled name values
    nearly all the DoD plugins want to use this kind of output. Using
    styledHash and converting from the format here works somewhat ok,
    buuuuut breaks down if you want to e.g. have duplicate keys.
    
    For example, here's the current (broken) output of `heroku
    cassandra:info`:
    
    ```bash
    === CASSANDRA_URL
    Name:        cassandra-hi-jeff-5562
    Created:     2015-09-42 09:42 UTC
    Plan:        Alpha Medium
    Status:      available
    Connections: 368
    Tables:      9001
    Data Size:   1.35 PB
    
    == Reads:
    Throughput:  849.37
    Latency:     50th 0.24ms
                 95th 0.24ms
                 99th 0.92ms
    
    == Writes:
    Throughput:  849.37
    Latency:     50th 0.24ms
                 95th 0.24ms
                 99th 0.92ms
    ```
    
    The duplicated "throughput" and latency columns happen because styleHash
    doesn't allow for keys with the same name.

--- test/styled.js
+++ test/styled.js
@@ -15,3 +15,17 @@ describe('styledHash', function () {
     expect(cli.color.stripColor(cli.stdout)).to.contain('=== MyApp\n');
   });
 });
+
+describe('styledNameValues', function () {
+  it('prints out a styled set of name values', function () {
+    cli.styledHash([{name: 'Name', values: ["myapp"]}, {name: 'Collaborators', values: ["user1@example.com", "user2@example.com"]}]);
+    expect(cli.color.stripColor(cli.stdout)).to.contain('Collaborators: user1@example.com\n' +
+     '               user2@example.com\n' +
+     'Name:          myapp\n');
+  });
+
+  it('prints out a styled header', function () {
+    cli.styledHeader('MyApp');
+    expect(cli.color.stripColor(cli.stdout)).to.contain('=== MyApp\n');
+  });
+});
