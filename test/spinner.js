b00b958b403b9f69eb0cc1f19c3121c8f7201ac1
Jeff Dickey <dickeyxxx@gmail.com>
2016-04-13 13:15:50 -0400

    fix test output with spinner under tests (#59)
    * fix test output with spinner under tests
    
    * added code coverage
    
    * added spinner test
    
    * remove docs badge
    
    * added test for cli.action

---
+++ test/spinner.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let Spinner     = require('../lib/spinner');
+let hookStd     = require('hook-std');
+let mockConsole = require('../lib/console');
+let chalk       = require('chalk');
+
+describe('spinner', () => {
+  function readOutput(spinner, callback) {
+    let out = '';
+
+    const unhook = hookStd.stderr({silent: true}, output => {
+      out += output;
+    });
+
+    spinner.start();
+    spinner.stop();
+    unhook();
+
+    callback(out);
+  }
+
+  beforeEach(() => {
+    mockConsole.mock(false);
+    process.stderr.isTTY = true;
+    process.stderr.clearLine = function () {};
+    process.stderr.cursorTo = function () {};
+  });
+
+  afterEach(() => {
+    mockConsole.mock();
+  });
+
+  it('main', () => {
+    const spinner = new Spinner({text: 'foo'});
+
+    spinner.enabled = true;
+
+    readOutput(spinner, (output) => {
+      expect(chalk.stripColor(output)).to.eq('foo ⣾foo');
+    });
+  });
+});
