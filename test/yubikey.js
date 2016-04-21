29cc0558d71ddeb3c7d6b9ac71b20b455c9a49b3
Jeff Dickey <dickeyxxx@gmail.com>
2016-04-21 07:28:15 -0700

    added yubikey test

---
+++ test/yubikey.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let sinon = require('sinon');
+let cp = require('child_process');
+
+describe('yubikey', () => {
+  let mock, yubikey;
+  beforeEach(() => {
+    mock = sinon.mock(require('child_process'));
+    yubikey = require('../lib/yubikey');
+    yubikey.platform = 'darwin';
+  });
+  afterEach(() => {
+    mock.verify();
+    mock.restore();
+  });
+
+  it('turns yubikey on', () => {
+    mock.expects('execSync').withExactArgs(`osascript -e 'if application "yubiswitch" is running then tell application "yubiswitch" to KeyOn'`, { stdio: "inherit" }).once();
+    yubikey.enable();
+  });
+
+  it('turns yubikey off', () => {
+    mock.expects('execSync').withExactArgs(`osascript -e 'if application "yubiswitch" is running then tell application "yubiswitch" to KeyOff'`, { stdio: "inherit" }).once();
+    yubikey.disable();
+  });
+});
