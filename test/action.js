5af8992cec692710769d432111c7dede60f13665
Jeff Dickey <dickeyxxx@gmail.com>
2016-04-21 08:45:33 -0700

    action.warn improvements

--- test/action.js
+++ test/action.js
@@ -6,13 +6,20 @@ describe('action', function () {
   beforeEach(() => cli.mockConsole());
 
   it('shows message', function () {
-    return cli.action('foobar', Promise.resolve())
-    .then(() => expect(cli.stderr, 'to equal', 'foobar... done\n'));
+    return cli.action('working', Promise.resolve())
+    .then(() => expect(cli.stderr, 'to equal', 'working... done\n'));
+  });
+
+  it('warns', function () {
+    let p = cli.action('working', new Promise((resolve) => process.nextTick(() => resolve())))
+    .then(() => expect(cli.stderr, 'to equal', 'working... !\n ▸    warning!\nworking... done\n'));
+    cli.action.warn('warning!');
+    return p;
   });
 
   it('errors', function () {
-    return expect(cli.action('foobar', Promise.reject(new Error('oh noes'))),
+    return expect(cli.action('working', Promise.reject(new Error('oh noes'))),
                   'to be rejected with', {message: 'oh noes'})
-    .then(() => expect(cli.stderr, 'to equal', 'foobar... !!!\n'));
+    .then(() => expect(cli.stderr, 'to equal', 'working... !!!\n'));
   });
 });
