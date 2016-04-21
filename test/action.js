ba8cb2d8013d7e2bed7543727aefe29f253da298
Jeff Dickey <dickeyxxx@gmail.com>
2016-04-21 08:22:49 -0700

    testing action

--- test/action.js
+++ test/action.js
@@ -1,12 +1,18 @@
 'use strict';
 
-let expect = require('chai').expect;
+let expect = require('unexpected');
 
 describe('action', function () {
   beforeEach(() => cli.mockConsole());
 
   it('shows message', function () {
     return cli.action('foobar', Promise.resolve())
-    .then(() => expect(cli.stderr).to.equal('foobar... done\n'));
+    .then(() => expect(cli.stderr, 'to equal', 'foobar... done\n'));
+  });
+
+  it('errors', function () {
+    return expect(cli.action('foobar', Promise.reject(new Error('oh noes'))),
+                  'to be rejected with', {message: 'oh noes'})
+    .then(() => expect(cli.stderr, 'to equal', 'foobar... !!!\n'));
   });
 });
