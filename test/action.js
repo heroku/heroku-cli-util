95b947ce3e2efca589b45cba835792d24a45b694
Jeff Dickey <dickeyxxx@gmail.com>
2016-05-19 22:18:00 -0700

    use ansi escapes to render spinner more cleanly

--- test/action.js
+++ test/action.js
@@ -3,25 +3,44 @@
 
 const expect = require('unexpected')
 const cli = require('..')
+const hookStd = require('hook-std')
 
 describe('action', function () {
   beforeEach(() => cli.mockConsole())
 
   it('shows message', function () {
+    let out = ''
+    const unhook = hookStd.stderr({silent: true}, output => { out += output })
+
     return cli.action('working', Promise.resolve())
-      .then(() => expect(cli.stderr, 'to equal', 'working... done\n'))
+    .then(() => {
+      unhook()
+      expect(out, 'to equal', 'working... done\n')
+    })
   })
 
   it('warns', function () {
+    let out = ''
+    const unhook = hookStd.stderr({silent: true}, output => { out += output })
+
     let p = cli.action('working', new Promise((resolve) => process.nextTick(() => resolve())))
-      .then(() => expect(cli.stderr, 'to equal', 'working... !\n ▸    warning!\nworking... done\n'))
+      .then(() => {
+        unhook()
+        expect(out, 'to equal', 'working... !\n ▸    warning!\nworking... done\n')
+      })
     cli.action.warn('warning!')
     return p
   })
 
   it('errors', function () {
+    let out = ''
+    const unhook = hookStd.stderr({silent: true}, output => { out += output })
+
     return expect(cli.action('working', Promise.reject(new Error('oh noes'))),
       'to be rejected with', {message: 'oh noes'})
-      .then(() => expect(cli.stderr, 'to equal', 'working... !!!\n'))
+      .then(() => {
+        unhook()
+        expect(out, 'to equal', 'working... !!!\n')
+      })
   })
 })
