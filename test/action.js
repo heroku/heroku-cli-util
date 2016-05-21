b4f6702b7a30b20da5c2c9d71157331a233772e4
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-05-20 19:15:20 -0700

    build fix

--- test/action.js
+++ test/action.js
@@ -3,44 +3,25 @@
 
 const expect = require('unexpected')
 const cli = require('..')
-const hookStd = require('hook-std')
 
 describe('action', function () {
   beforeEach(() => cli.mockConsole())
 
   it('shows message', function () {
-    let out = ''
-    const unhook = hookStd.stderr({silent: true}, output => { out += output })
-
     return cli.action('working', Promise.resolve())
-    .then(() => {
-      unhook()
-      expect(cli.color.stripColor(out), 'to equal', 'working... done\n')
-    })
+    .then(() => expect(cli.stderr, 'to equal', 'working... done\n'))
   })
 
   it('warns', function () {
-    let out = ''
-    const unhook = hookStd.stderr({silent: true}, output => { out += output })
-
     let p = cli.action('working', new Promise((resolve) => process.nextTick(() => resolve())))
-      .then(() => {
-        unhook()
-        expect(cli.color.stripColor(out), 'to equal', 'working... !\n ▸    warning!\nworking... done\n')
-      })
+      .then(() => expect(cli.stderr, 'to equal', 'working... !\n ▸    warning!\nworking... done\n'))
     cli.action.warn('warning!')
     return p
   })
 
   it('errors', function () {
-    let out = ''
-    const unhook = hookStd.stderr({silent: true}, output => { out += output })
-
     return expect(cli.action('working', Promise.reject(new Error('oh noes'))),
       'to be rejected with', {message: 'oh noes'})
-      .then(() => {
-        unhook()
-        expect(cli.color.stripColor(out), 'to equal', 'working... !!!\n')
-      })
+      .then(() => expect(cli.stderr, 'to equal', 'working... !!!\n'))
   })
 })
