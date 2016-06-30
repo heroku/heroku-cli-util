ffe445c89e085b37572561fd73df0a3b851fa80b
Jeff Dickey <dickeyxxx@gmail.com>
2016-06-30 16:41:57 -0700

    use global context object to initialize heroku client

--- test/preauth.js
+++ test/preauth.js
@@ -2,18 +2,16 @@
 /* globals describe it */
 
 const cli = require('..')
-const Heroku = require('heroku-client')
 const nock = require('nock')
 
 describe('preauth', function () {
   it('makes a POST to /apps/myapp/pre-authorizations', function () {
-    let heroku = new Heroku()
     nock('https://api.heroku.com', {
       reqheaders: {'Heroku-Two-Factor-Code': '2fa key'}
     })
       .put('/apps/myapp/pre-authorizations')
       .reply(200, {})
 
-    return cli.preauth('myapp', heroku, '2fa key')
+    return cli.preauth('myapp', '2fa key')
   })
 })
