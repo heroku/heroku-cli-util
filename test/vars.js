ba26e6e606b99f8c21a53e3560fb5dd4a0aeccae
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-03-17 11:00:10 -0700

    read hosts from env vars (#91)
    * read hosts from env vars
    
    * fix up vars
    
    * removed unused vars

---
+++ test/vars.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const Vars = require('../lib/vars').constructor
+const expect = require('unexpected')
+
+describe('vars', () => {
+  it('sets vars by default', () => {
+    const vars = new Vars({})
+    expect(vars.host, 'to equal', 'heroku.com')
+    expect(vars.apiHost, 'to equal', 'api.heroku.com')
+    expect(vars.apiUrl, 'to equal', 'https://api.heroku.com')
+  })
+
+  it('respects HEROKU_HOST', () => {
+    const vars = new Vars({HEROKU_HOST: 'customhost'})
+    expect(vars.host, 'to equal', 'customhost')
+    expect(vars.apiHost, 'to equal', 'api.customhost')
+    expect(vars.apiUrl, 'to equal', 'https://api.customhost')
+  })
+
+  it('respects HEROKU_HOST as url', () => {
+    const vars = new Vars({HEROKU_HOST: 'https://customhost'})
+    expect(vars.host, 'to equal', 'https://customhost')
+    expect(vars.apiHost, 'to equal', 'customhost')
+    expect(vars.apiUrl, 'to equal', 'https://customhost')
+  })
+})
+
