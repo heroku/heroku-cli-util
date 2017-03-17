ab09a2e7bf37ac314324bddab9066d729a0f4b49
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-03-17 10:25:13 -0700

    removed unused vars

--- test/vars.js
+++ test/vars.js
@@ -9,13 +9,6 @@ describe('vars', () => {
     expect(vars.host, 'to equal', 'heroku.com')
     expect(vars.apiHost, 'to equal', 'api.heroku.com')
     expect(vars.apiUrl, 'to equal', 'https://api.heroku.com')
-    expect(vars.gitHost, 'to equal', 'heroku.com')
-    expect(vars.httpGitHost, 'to equal', 'git.heroku.com')
-    expect(vars.gitPrefixes, 'to equal', [
-      'git@heroku.com:',
-      'ssh://git@heroku.com/',
-      'https://git.heroku.com/'
-    ])
   })
 
   it('respects HEROKU_HOST', () => {
@@ -23,13 +16,6 @@ describe('vars', () => {
     expect(vars.host, 'to equal', 'customhost')
     expect(vars.apiHost, 'to equal', 'api.customhost')
     expect(vars.apiUrl, 'to equal', 'https://api.customhost')
-    expect(vars.gitHost, 'to equal', 'customhost')
-    expect(vars.httpGitHost, 'to equal', 'git.customhost')
-    expect(vars.gitPrefixes, 'to equal', [
-      'git@customhost:',
-      'ssh://git@customhost/',
-      'https://git.customhost/'
-    ])
   })
 
   it('respects HEROKU_HOST as url', () => {
@@ -37,13 +23,6 @@ describe('vars', () => {
     expect(vars.host, 'to equal', 'https://customhost')
     expect(vars.apiHost, 'to equal', 'customhost')
     expect(vars.apiUrl, 'to equal', 'https://customhost')
-    expect(vars.gitHost, 'to equal', 'customhost')
-    expect(vars.httpGitHost, 'to equal', 'customhost')
-    expect(vars.gitPrefixes, 'to equal', [
-      'git@customhost:',
-      'ssh://git@customhost/',
-      'https://customhost/'
-    ])
   })
 })
 
