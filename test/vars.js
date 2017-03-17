863e1bdc47b965fc550cbfbfba30c700af9e6751
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-03-17 10:14:57 -0700

    fix up vars

--- test/vars.js
+++ test/vars.js
@@ -7,15 +7,43 @@ describe('vars', () => {
   it('sets vars by default', () => {
     const vars = new Vars({})
     expect(vars.host, 'to equal', 'heroku.com')
+    expect(vars.apiHost, 'to equal', 'api.heroku.com')
+    expect(vars.apiUrl, 'to equal', 'https://api.heroku.com')
     expect(vars.gitHost, 'to equal', 'heroku.com')
     expect(vars.httpGitHost, 'to equal', 'git.heroku.com')
+    expect(vars.gitPrefixes, 'to equal', [
+      'git@heroku.com:',
+      'ssh://git@heroku.com/',
+      'https://git.heroku.com/'
+    ])
   })
 
   it('respects HEROKU_HOST', () => {
+    const vars = new Vars({HEROKU_HOST: 'customhost'})
+    expect(vars.host, 'to equal', 'customhost')
+    expect(vars.apiHost, 'to equal', 'api.customhost')
+    expect(vars.apiUrl, 'to equal', 'https://api.customhost')
+    expect(vars.gitHost, 'to equal', 'customhost')
+    expect(vars.httpGitHost, 'to equal', 'git.customhost')
+    expect(vars.gitPrefixes, 'to equal', [
+      'git@customhost:',
+      'ssh://git@customhost/',
+      'https://git.customhost/'
+    ])
+  })
+
+  it('respects HEROKU_HOST as url', () => {
     const vars = new Vars({HEROKU_HOST: 'https://customhost'})
     expect(vars.host, 'to equal', 'https://customhost')
+    expect(vars.apiHost, 'to equal', 'customhost')
+    expect(vars.apiUrl, 'to equal', 'https://customhost')
     expect(vars.gitHost, 'to equal', 'customhost')
     expect(vars.httpGitHost, 'to equal', 'customhost')
+    expect(vars.gitPrefixes, 'to equal', [
+      'git@customhost:',
+      'ssh://git@customhost/',
+      'https://customhost/'
+    ])
   })
 })
 
