343ce6bd36f2bd8a5d73a620ec02db8b0f0682c3
Matthew Origer <moriger@heroku.com>
2017-03-24 14:27:14 -0500

    recommend cmd.exe on win32

--- test/prompt.js
+++ test/prompt.js
@@ -3,6 +3,8 @@
 
 let expect = require('unexpected')
 let prompt = require('../lib/prompt').prompt
+let os = require('os')
+let sinon = require('sinon')
 
 let isTTY
 
@@ -35,5 +37,10 @@ describe('login', function () {
   it('throws error when prompt masked and not tty', function () {
     process.stdin.isTTY = false
     return expect(prompt('foo', {mask: true}), 'to be rejected with', 'CLI needs to prompt for foo but stdin is not a tty.')
+  })
+  it('recommends using cmd.exe on windows', () => {
+    process.stdin.isTTY = false
+    os.platform = sinon.stub().returns('win32')
+    return expect(prompt('foo', {hide: true}), 'to be rejected with', 'CLI needs to prompt for foo but stdin is not a tty. This is likely a problem with your shell sofware. On win32 plaftorms, we recommend using cmd.exe')
   })
 })
