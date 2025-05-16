2f01a7645ecb6ccf8026a3987d65c72a8e8bfa54
Eric Black <eblack@salesforce.com>
2025-05-16 12:16:11 -0700

    reimplement wait, fix databases test

--- test/unit/ux/wait.test.ts
+++ test/unit/ux/wait.test.ts
@@ -1,12 +1,12 @@
 import {expect} from 'chai'
 
-// import {wait} from '../../../src/ux/wait.js'
+import {wait} from '../../../src/ux/wait.js'
 
-describe.skip('wait', function () {
+describe('wait', function () {
   it('should actually wait for at least the specified period of time', async function () {
     const duration = 200
     const start = Date.now()
-    // await wait(duration)
+    await wait(duration)
     const elapsed = Date.now() - start
     // Allow a small margin for timing inaccuracy
     expect(elapsed).to.be.at.least(duration - 20)
