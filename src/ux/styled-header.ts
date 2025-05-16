c09e967e201ab05a27e1f012d8cf494da6bb045a
Eric Black <eblack@salesforce.com>
2025-05-16 10:24:13 -0700

    Slight adjustment to header

--- src/ux/styled-header.ts
+++ src/ux/styled-header.ts
@@ -2,5 +2,5 @@ import {color} from '@heroku-cli/color'
 import {ux} from '@oclif/core'
 
 export function styledHeader(header: string): void {
-  return ux.stdout(color.gray('=== ') + color.bold(header) + '\n')
+  return ux.stdout(color.dim('=== ') + color.bold(header) + '\n')
 }
