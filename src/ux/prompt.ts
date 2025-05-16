bb512da54319710ab1534feffd2fcc1ab1421a01
Eric Black <eblack@salesforce.com>
2025-05-16 14:46:26 -0700

    Add type, set type if provided

--- src/ux/prompt.ts
+++ src/ux/prompt.ts
@@ -1,11 +1,17 @@
 import inquirer from 'inquirer'
 
-export async function prompt(name: string, options?: {default?: string; required?: boolean; type?: string}): Promise<string> {
+type PromptOptions = {
+  default?: string
+  required?: boolean
+  type?: string
+}
+
+export async function prompt(name: string, options?: PromptOptions): Promise<string> {
   const {answer} = await (inquirer as any).prompt([{
     default: options?.default,
     message: name,
     name: 'answer',
-    type: 'input',
+    type: options?.type ?? 'input',
     validate: options?.required ? (input: string) => input.length > 0 || 'This field is required' : undefined,
   }])
   return answer
