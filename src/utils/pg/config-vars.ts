65d24086a88c55597e24817704a719be8a21e022
Santiago Bosio <santiago.bosio@gmail.com>
2025-08-01 19:20:37 -0300

    Refactored config var functions, added tests

--- src/utils/pg/config-vars.ts
+++ src/utils/pg/config-vars.ts
@@ -1,42 +1,62 @@
 import type {HTTP} from '@heroku/http-call'
 import type {APIClient} from '@heroku-cli/command'
-import type {AddOnAttachment} from '@heroku-cli/schema'
 
 import {color} from '@heroku-cli/color'
-import {ux} from '@oclif/core'
 
-import type {AddOnAttachmentWithConfigVarsAndPlan} from '../../types/pg/data-api.js'
+import type {ExtendedAddonAttachment} from '../../types/pg/data-api.js'
 
-const responseByAppId: Map<string, Promise<HTTP<Record<string, string>>>> = new Map()
+/*
+ * Cache of app config vars.
+ */
+export const configVarsByAppIdCache: Map<string, Promise<HTTP<Record<string, string>>>> = new Map()
 
-export async function getConfig(heroku: APIClient, app: string): Promise<Record<string, string> | undefined> {
-  if (!responseByAppId.has(app)) {
-    const promise = heroku.get<Record<string, string>>(`/apps/${app}/config-vars`)
-    responseByAppId.set(app, promise)
+/*
+ * Returns the app's config vars as a record of key-value pairs, either from the cache or from the API.
+ */
+export async function getConfig(heroku: APIClient, appId: string): Promise<Record<string, string>> {
+  let promise = configVarsByAppIdCache.get(appId)
+
+  if (!promise) {
+    promise = heroku.get<Record<string, string>>(`/apps/${appId}/config-vars`)
+    configVarsByAppIdCache.set(appId, promise)
   }
 
-  const result = await responseByAppId.get(app)
-  return result?.body
+  const {body: config} = await promise
+  return config
 }
 
-export function getConfigVarName(configVars: string[]): string {
-  const connStringVars = configVars.filter(cv => (cv.endsWith('_URL')))
-  if (connStringVars.length === 0) throw new Error('Database URL not found for this addon')
-  return connStringVars[0]
+/**
+ * Returns the attachment's first config var name that has a `_URL` suffix, expected to be the name of the one
+ * that contains the database URL connection string.
+ */
+export function getConfigVarName(configVarNames: ExtendedAddonAttachment['config_vars']): string {
+  const urlConfigVarNames = configVarNames.filter(cv => (cv.endsWith('_URL')))
+  if (urlConfigVarNames.length === 0) throw new Error('Database URL not found for this addon')
+  return urlConfigVarNames[0]
 }
 
-export function getConfigVarNameFromAttachment(attachment: Required<{
-  addon: AddOnAttachmentWithConfigVarsAndPlan
-} & AddOnAttachment>, config: Record<string, string> = {}): string {
-  const configVars = attachment.addon.config_vars?.filter((cv: string) => config[cv]?.startsWith('postgres://')) ?? []
-  if (configVars.length === 0) {
-    ux.error(`No config vars found for ${attachment.name}; perhaps they were removed as a side effect of ${color.cmd('heroku rollback')}? Use ${color.cmd('heroku addons:attach')} to create a new attachment and then ${color.cmd('heroku addons:detach')} to remove the current attachment.`)
+/**
+ * This function returns the config var name that contains the database URL connection string for the given
+ * attachment, based on the contents of the app's config vars.
+ */
+export function getConfigVarNameFromAttachment(attachment: ExtendedAddonAttachment, config: Record<string, string> = {}): string {
+  // Handle the case where no attachment config var names remain after filtering out those that don't contain a
+  // database URL connection string in the app's config vars.
+  const connStringConfigVarNames = attachment.config_vars.filter((cvn: string) => config[cvn]?.startsWith('postgres://')) ?? []
+  if (connStringConfigVarNames.length === 0) {
+    throw new Error(
+      `No config vars found for ${attachment.name}; perhaps they were removed as a side effect of` +
+      `${color.cmd('heroku rollback')}? Use ${color.cmd('heroku addons:attach')} to create a new attachment and ` +
+      `then ${color.cmd('heroku addons:detach')} to remove the current attachment.`
+    )
   }
 
+  // Generate the default config var name and return it if it contains a database URL connection string.
   const configVarName = `${attachment.name}_URL`
-  if (configVars.includes(configVarName) && configVarName in config) {
+  if (connStringConfigVarNames.includes(configVarName) && configVarName in config) {
     return configVarName
   }
 
-  return getConfigVarName(configVars)
+  // Return the first config var name that has a `_URL` suffix. This might not be needed at all anymore.
+  return getConfigVarName(connStringConfigVarNames)
 }
